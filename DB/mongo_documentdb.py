7b920cae1cf0f22c3deacba2b0165c75e5ccc637
farrelarrizal <mfarrel1301@gmail.com>
2022-12-15 13:00:50 +0700

    add env for mongo db

---
+++ DB/mongo_documentdb.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import yaml
+from flask import jsonify
+
+with open("./app.yaml", "r") as stream:
+    try:
+        env = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+
+HOST_URI = str(env["env_variables"]["HOST_URI"])
